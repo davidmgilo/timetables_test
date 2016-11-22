421b1002dae6f8daf536e423e7aad85f60743a52
David Martinez <davidmgilo@gmail.com>
2016-11-22 18:23:04 +0100

    done php artisan vendor:publish --tag=scool_timetables

---
+++ tests/TimetablesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * Class MisTestos
+ */
+class TimetablesTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test to know if there are shifts
+     * @test
+     * @group failing
+     */
+    public function it_shows_shifts()
+    {
+        // Preparació -> amb un new, no es pot modificar i, per tant, no es pot testejar/preparar.
+        // Execució
+        // Assertions: comprovacions/ confirmacions
+        // Seeds/Models -> codi de laravel, no cal provar-lo.
+        $this->assertTrue(true);
+    }
+
+}
