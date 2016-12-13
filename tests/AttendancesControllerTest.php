d1fb5e7aa3e048eac3be14108842779336d6aa85
David Martinez <davidmgilo@gmail.com>
2016-12-13 17:24:25 +0100

    test index - no assegura que sigui no buit - factory per poder comprovar-ho

--- tests/AttendancesControllerTest.php
+++ tests/AttendancesControllerTest.php
@@ -20,14 +20,24 @@ class AttendancesControllerTest extends TestCase
     public function testIndex()
     {
 //        dd(route('attendances.index'));
+//        $attendances = factory(\Scool\Timetables\Models\Attendance::class,50)->create();
         $user = factory(App\User::class)->create();
         $this->actingAs($user);
         $this->get('attendances');
         $this->assertResponseOk();
 
+        $this->assertViewHas('attendances');
+
+        $attendances = $this->response->getOriginalContent()->getData()['attendances'];
+        $this->assertInstanceOf(\Illuminate\Database\Eloquent\Collection::class , $attendances);
 
         //1) Preparació
         //2) Execució
         //3) Assertions
+    }
+
+    public function testStore()
+    {
+        
     }
 }
