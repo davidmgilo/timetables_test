33fb9b05c904b6afac5f9884ec5b960daca8e03e
David Martinez <davidmgilo@gmail.com>
2016-12-13 16:45:51 +0100

    created test for controller

---
+++ tests/AttendancesControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class AttendancesControllerTest
+ */
+class AttendancesControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        
+    }
+}
