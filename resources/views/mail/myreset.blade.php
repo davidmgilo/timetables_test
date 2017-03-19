e9192eb165b9eecad1e148f11250decaa479f8a8
David Martínez Gilo <davidmgilo@gmail.com>
2017-03-19 20:48:20 +0100

    mail

--- resources/views/mail/myreset.blade.php
+++ resources/views/mail/myreset.blade.php
@@ -1,13 +1,20 @@
 @component('mail::message')
-# Introduction
+# Reset Password
 
-The body of your message.
+Hello,
+
+You are receiving this email because we received a password reset request for your account.
 
 @component('mail::button', ['url' => $url])
 Reset Password
 @endcomponent
 
+If you did not request a password reset, no further action is required.
+
 Thanks,<br>
 {{ config('app.name') }}
-<a href="{{ $url }}">{{ $url }}</a>
+@component('mail::subcopy')
+If you’re having trouble clicking the Reset Password button, copy and paste the URL below
+into your web browser: <a href="{{ $url }}">{{ $url }}</a>
+@endcomponent
 @endcomponent
