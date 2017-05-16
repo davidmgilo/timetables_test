83e04d0ff5e89ab82730d1afe1995bccad751601
David Martinez <davidmgilo@gmail.com>
2017-05-16 15:57:18 +0200

    try - not installed

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Class PdfController
+ * @package App\Http\Controllers
+ */
 class PdfController extends Controller
 {
     public function users()
     {
-        
+        return 'todo';
     }
 
     public function user()
     {
-        
+        //
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML('<h1>Hello World!</h1>');
+
+        return $pdf->stream('user');
     }
 }
